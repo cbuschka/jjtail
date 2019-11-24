f89aa01e3047b035680c852db66edb19c62f1971
conni <c.buschka@arcusx.com>
2019-11-24 11:27:52 +0100

    A quick hack.

---
+++ jjtail.py
@@ -0,0 +1,13 @@
+#!/bin/python3.7
+
+import json
+import sys
+from sh import tail
+
+LEVELS = ["???", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "INFO", "eight", "nine", "ten"]
+
+for line in tail("-f", sys.argv[1], _iter=True):
+  map = json.loads(line)
+  map["level"] = LEVELS[int(map.get("level","-1"))+1]
+  print("{timestamp} [{level}] {_app_name}@{host} [{_thread_name}] {short_message}".format(**map))
+
