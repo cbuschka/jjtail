02494c6566895c4d1d91e44f02b35aa37ef6661c
conni <c.buschka@arcusx.com>
2019-11-30 17:39:46 +0100

    Added human readable timestamp.

--- jjtail.py
+++ jjtail.py
@@ -2,6 +2,7 @@
 
 import json
 import sys
+import time
 from sh import tail
 
 LEVELS = ["???", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "INFO", "eight", "nine", "ten"]
@@ -9,4 +10,5 @@ LEVELS = ["???", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "INFO", "
 for line in tail(*sys.argv[1:], _iter=True):
   map = json.loads(line)
   map["level"] = LEVELS[int(map.get("level","-1"))+1]
+  map["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S%z', time.gmtime(int(map["timestamp"])))
   print("{timestamp} [{level}] {_app_name}@{host} [{_thread_name}] {short_message}".format(**map))
