78fde2447b533dec8103f8a3e05e1ed1d6e91dae
conni <c.buschka@arcusx.com>
2019-11-30 17:58:43 +0100

    Removed _app_name.

--- jjtail.py
+++ jjtail.py
@@ -11,4 +11,4 @@ for line in tail(*sys.argv[1:], _iter=True):
   map = json.loads(line)
   map["level"] = LEVELS[int(map.get("level","-1"))+1]
   map["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S%z', time.gmtime(int(map["timestamp"])))
-  print("{timestamp} [{level}] {_app_name}@{host} [{_thread_name}] {short_message}".format(**map))
+  print("{timestamp} [{level}] {host} [{_thread_name}] {short_message}".format(**map))
