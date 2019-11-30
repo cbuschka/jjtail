5b37302fe371bb990d77f2e0862129482be2d12d
conni <c.buschka@arcusx.com>
2019-11-30 18:03:50 +0100

    Improved format.

--- jjtail.py
+++ jjtail.py
@@ -10,5 +10,5 @@ LEVELS = ["???", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "INFO", "
 for line in tail(*sys.argv[1:], _iter=True):
   map = json.loads(line)
   map["level"] = LEVELS[int(map.get("level","-1"))+1]
-  map["timestamp"] = time.strftime('%Y-%m-%dT%H:%M:%S%z', time.gmtime(int(map["timestamp"])))
-  print("{timestamp} [{level}] {host} [{_thread_name}] {short_message}".format(**map))
+  map["timestamp"] = time.strftime('%Y-%m-%d %H:%M:%S%z', time.gmtime(int(map["timestamp"])))
+  print("{timestamp} {host} [{_thread_name}] {level}  {short_message}".format(**map))
