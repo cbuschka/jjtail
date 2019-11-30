405901059e78740580e5b98815f605e8573c3f11
conni <c.buschka@arcusx.com>
2019-11-30 17:27:48 +0100

    Added passing params to tail.

--- jjtail.py
+++ jjtail.py
@@ -1,4 +1,4 @@
-#!/bin/python3.7
+#!/usr/bin/python3
 
 import json
 import sys
@@ -6,8 +6,7 @@ from sh import tail
 
 LEVELS = ["???", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "DEBUG", "INFO", "eight", "nine", "ten"]
 
-for line in tail("-f", sys.argv[1], _iter=True):
+for line in tail(*sys.argv[1:], _iter=True):
   map = json.loads(line)
   map["level"] = LEVELS[int(map.get("level","-1"))+1]
   print("{timestamp} [{level}] {_app_name}@{host} [{_thread_name}] {short_message}".format(**map))
-
